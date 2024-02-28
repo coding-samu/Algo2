ce4e27d3ef5774a2908039b66d07afe06bc37b6f
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-02-28 11:03:06 +0100

    Modificata funzione semiprimo in testSemiprimo.py

--- testSemiprimo.py
+++ testSemiprimo.py
@@ -4,7 +4,9 @@ from math import sqrt
 def semiprimo(n):
     # Calcola il limite superiore per il ciclo
     bound = int(sqrt(n))
-    temp = 0
-    for i in range(2, bound):
+    count = 0
+    for i in range(2, bound+1):
         if n % i == 0:
-            temp = n // i
+            count += 1
+    return count == 1
+            
