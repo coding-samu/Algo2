4ec8095b7c76defb10a2a097171a0a79fe9b3ca2
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-03-05 15:53:44 +0100

    Modificato metodo orienta_grafo_completo

--- completeGraph.py
+++ completeGraph.py
@@ -6,6 +6,7 @@ def generate_complete_graph(n: int) -> list[list[int]]:
                 G[i].append(j)
     return G
 
+
 """
 Sia G un grafo completo con m archi. Descrivere un algoritmo che dato
 G rappresentato tramite liste di adiacenza in O(m) ne orienta gli archi
@@ -13,23 +14,23 @@ producendo un grafo diretto aciclico
 """
 def orienta_grafo_completo(G: list[list[int]]) -> list[list[int]]:
     n = len(G)  # Numero di nodi
-    archi_orientati = []  # Lista per memorizzare gli archi orientati
+    archi_orientati = [[] for _ in range(len(G))]  # Lista per memorizzare gli archi orientati
 
     # Iteriamo su tutte le coppie di nodi distinti
     for u in range(n):
         for v in range(u + 1, n):
             # Orientiamo l'arco u → v se u è adiacente a v
             if v in G[u]:
-                archi_orientati.append([u, v])
+                archi_orientati[u].append(v)
             # Altrimenti, orientiamo l'arco v → u
             else:
-                archi_orientati.append([v, u])
+                archi_orientati[v].append(u)
 
     return archi_orientati
 
 
 # Esempio
-G = generate_complete_graph(4)
+G = generate_complete_graph(5)
 print(G)
 G = orienta_grafo_completo(G)
 print(G)
