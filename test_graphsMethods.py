7f4e223d0bfa50733f3f0740e2f18699a364f41e
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-03-21 15:44:01 +0100

    updated tests

--- test_graphsMethods.py
+++ test_graphsMethods.py
@@ -66,48 +66,119 @@ def test_find_diameter_dfs():
 test_find_diameter_dfs()
 
 ### Test case for bfs same distance ###
-#G = [[1,5],[2],[3],[4],[],[2,4],[2]]
-#print(bfs_same_distance(6, 5, G))
+def test_bfs_same_distance():
+    # Test case 1: Simple graph with 7 nodes
+    G = [[1,5],[2],[3],[4],[],[2,4],[2]]
+    assert bfs_same_distance(6, 5, G) == [0, 1, 2, 3]
+
+    print("All test cases passed!")
+
+test_bfs_same_distance()
 
 ### Test case for nodi di articolazione ###
-#G = [[3,4,5,8],[2,7],[1,6,7],[0,4,7],[0,3],[0,8],[2],[1,2,3],[0,5]]
-#print(nodi_di_articolazione(G))
+def test_nodi_di_articolazione():
+    # Test case 1: Simple graph with 9 nodes
+    G = [[3,4,5,8],[2,7],[1,6,7],[0,4,7],[0,3],[0,8],[2],[1,2,3],[0,5]]
+    assert nodi_di_articolazione(G) == [0, 2, 3, 7]
+
+    print("All test cases passed!")
+
+test_nodi_di_articolazione()
 
 ### Test case for delete sequence ###
-#G = [[3,4,5,8],[2,7],[1,6,7],[0,4,7],[0,3],[0,8],[2],[1,2,3],[0,5]]
-#print(delete_sequence(G))
+def test_delete_sequence():
+    # Test case 1: Simple graph with 9 nodes
+    G = [[3,4,5,8],[2,7],[1,6,7],[0,4,7],[0,3],[0,8],[2],[1,2,3],[0,5]]
+    assert delete_sequence(G) == [4, 6, 2, 1, 7, 3, 8, 5, 0]
+
+    print("All test cases passed!")
+
+test_delete_sequence()
 
 ### Test case for find bridges ###
-#G = [[3,4,5,8],[2,7],[1,6,7],[0,4,7],[0,3],[0,8],[2],[1,2,3],[0,5]]
-#print(find_bridges(G))
+def test_find_bridges():
+    # Test case 1: Simple graph with 9 nodes
+    G = [[3,4,5,8],[2,7],[1,6,7],[0,4,7],[0,3],[0,8],[2],[1,2,3],[0,5]]
+    assert find_bridges(G) == [(2, 6), (3, 7)]
+
+    print("All test cases passed!")
+
+test_find_bridges()
 
 ### Test case for find path all ###
-#G = [[1,3,4],[0,2],[1,3,4],[0,2,4],[0,3,2]]
-#print(find_path_all(0, G))
+def test_find_path_all():
+    # Test case 1: Simple graph with 5 nodes
+    G = [[1,3,4],[0,2],[1,3,4],[0,2,4],[0,3,2]]
+    assert find_path_all(0, G) == [0, 1, 2, 3, 4, 3, 2, 4, 2, 1, 0, 3, 0, 4, 0]
+
+    print("All test cases passed!")
+
+test_find_path_all()
 
 ### Test case for count edges ###
-#G = [[1,2],[3],[3],[4,5],[5],[6],[1]]
-#print(count_edges(G))  # Output: (1, 1, 1)
+def test_count_edges():
+    # Test case 1: Simple graph with 7 nodes
+    G = [[1,2],[3],[3],[4,5],[5],[6],[1]]
+    assert count_edges(G) == (1, 1, 1)
+
+    print("All test cases passed!")
+
+test_count_edges()
 
 ### Test case for ordinamento topologico dfs ###
-#print(ordinamento_topologico_dfs([[1,4,5],[2],[3],[4],[],[2],[2]]))
+def test_ordinamento_topologico_dfs():
+    # Test case 1: Simple graph with 7 nodes
+    G = [[1,4,5],[2],[3],[4],[],[2],[2]]
+    assert ordinamento_topologico_dfs(G) == [6, 0, 5, 1, 2, 3, 4]
+
+    print("All test cases passed!")
 
 ### Test case for generate complete graph ###
-#G = generate_complete_graph(4)
-#print(G)
+def test_generate_complete_graph():
+    # Test case 1: Simple graph with 4 nodes
+    G = generate_complete_graph(4)
+    assert G == [[1, 2, 3], [0, 2, 3], [0, 1, 3], [0, 1, 2]]
+
+    print("All test cases passed!")
+
+test_generate_complete_graph()
 
 ### Test case for orienta grafo completo ###
-#G = orienta_grafo_completo(G)
-#print(G)
+def test_orienta_grafo_completo():
+    # Test case 1: Simple graph with 4 nodes
+    G = orienta_grafo_completo([[1, 2, 3], [0, 2, 3], [0, 1, 3], [0, 1, 2]])
+    assert G == [[1, 2, 3], [2, 3], [3], []]
+
+    print("All test cases passed!")
+
+test_orienta_grafo_completo()
 
 ### Test case for orient all ###
-#G = [[1],[3],[1,4],[4],[2,3],[4]]
-#print(orient_all(0, G, [0]*6))
+def test_orient_all():
+    # Test case 1: Simple graph with 6 nodes
+    G = [[1],[3],[1,4],[4],[2,3],[4]]
+    assert orient_all(0, G, [0]*6) == [[1], [3], [1, 4], [], [3], [4]]
+
+    print("All test cases passed!")
+
+test_orient_all()
 
 ### Test case for conta pozzi da nodo ###
-#G = [[2,9],[0,2,5,7],[],[4,6],[10],[],[9],[8],[],[10],[3]]
-#print(conta_pozzi_da_nodo(1, G, [0]*11))
+def test_conta_pozzi_da_nodo():
+    # Test case 1: Simple graph with 11 nodes
+    G = [[2,9],[0,2,5,7],[],[4,6],[10],[],[9],[8],[],[10],[3]]
+    assert conta_pozzi_da_nodo(1, G, [0]*11) == 3
+
+    print("All test cases passed!")
+
+test_conta_pozzi_da_nodo()
 
 ### Test case for sort Topologico ### 
-#G = [[1,5,6],[2,6],[],[2,4,6],[5,6],[],[2,5]]
-#print(sortTopologico(G))
+def test_sortTopologico():
+    # Test case 1: Simple graph with 7 nodes
+    G = [[1,5,6],[2,6],[],[2,4,6],[5,6],[],[2,5]]
+    assert sortTopologico(G) == [3, 4, 0, 1, 6, 5, 2]
+
+    print("All test cases passed!")
+
+test_sortTopologico()
