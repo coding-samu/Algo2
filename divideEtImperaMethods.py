f37f921a6ab2b59ab6ced2ba2868c6a6a4dcb964
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-04-22 12:06:23 +0200

    added deterministic version of divide et impera method that return the element of A with rank k

--- divideEtImperaMethods.py
+++ divideEtImperaMethods.py
@@ -1,4 +1,5 @@
 from random import randint
+from math import ceil
 
 def selezione2(A,k):
     '''Restituisce l'elemento di rango k dove 1 <= k <= len(A)'''
@@ -35,3 +36,25 @@ def selezione2R(A,k):
         return perno
     else:
         return selezione2R(A2,k-len(A1)-1)
+    
+def selezione(A,k):
+    ''' Restituisce l'elemento di rango k dove 1 <= k <= len(A) 
+        scegliendo ogni volta il perno con la regola del mediano dei mediani
+    '''
+    if len(A) <= 120:
+        A.sort()
+        return A[k-1]
+    B = [sorted(A[5*i : 5*i+5])[2] for i in range(len(A)//5)]
+    perno = selezione(B,ceil(len(A)/10))
+    A1, A2 = [], []
+    for x in A:
+        if x < perno:
+            A1.append(x)
+        elif x > perno:
+            A2.append(x)
+    if len(A1) >= k:
+        return selezione(A1,k)
+    elif len(A1) == k-1:
+        return perno
+    else:
+        return selezione(A2,k-len(A1)-1)
