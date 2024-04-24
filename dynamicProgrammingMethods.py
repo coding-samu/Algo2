d1c80350699af1e08bcf4efe7f6d5ffc2c6c1ff7
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-04-24 09:25:27 +0200

    added riempiDisco method and test using dynamic programming

--- dynamicProgrammingMethods.py
+++ dynamicProgrammingMethods.py
@@ -5,3 +5,23 @@ def fib(n):
     for i in range(2,n):
         a,b = b,a+b
     return b
+
+def esI(A,C):
+    n = len(A)
+    T = [[0]*(C+1) for i in range(n+1)]
+    for i in range(1,n+1):
+        for c in range(C+1):
+            if c < A[i-1]:
+                T[i][c] = T[i-1][c]
+            else:
+                T[i][c] = max(T[i-1][c],T[i-1][c-A[i-1]]+A[i-1])
+    
+    valore = T[n][C]
+    sol = set()
+    i = n
+    while i > 0:
+        if T[i][valore] != T[i-1][valore]:
+            sol.add(i-1)
+            valore -= A[i-1]
+        i -= 1
+    return T[n][C],sol
--- test_dynamicProgrammingMethods.py
+++ test_dynamicProgrammingMethods.py
@@ -14,3 +14,15 @@ def test_fib():
     print("All test cases passed!")
 
 test_fib()
+
+def test_riempiDisco():
+    print("Running test cases for riempiDisco...")
+
+    # Test case 1: Simple values
+    A = [80,15,40,95,30,50,40,30]
+    C = 85
+    assert riempiDisco(A,C) == (85,{1,2,4})
+    
+    print("All test cases passed!")
+
+test_riempiDisco()
