9efd8132d3db56ab977050fddf794988b78d8082
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-04-26 12:58:38 +0200

    added method [to test]

--- dynamicProgrammingMethods.py
+++ dynamicProgrammingMethods.py
@@ -58,10 +58,32 @@ def max_sum_distance_two(A):
         T[i] = max(T[i-1],T[i-3]+A[i])
     return T[n-1]
 
-def piastrellamento_un_tipo(n):
-    m = max(3,n)
+
+def piastrellamento_di_tipo_A(n):
     T = [0]*(n+1)
     T[1], T[2] = 1, 2
     for i in range(3,n+1):
         T[i] = T[i-1] + T[i-2]
     return T[n]
+
+def piastrellamento_di_tipo_B(n):
+    T = [0]*(n+1)
+    if n < 4: return 0
+    T[4] = 2
+    for i in range(5,n+1):
+        T[i] = T[i-1]*2
+    return T[n]
+
+def piastrellamento_di_due_tipi(n):
+    T = [0]*(n+1)
+    if n == 0: return 0
+    if n == 1: return 1
+    if n == 2: return 2
+    T[1], T[2] = 1, 2
+    for i in range(3,n+1):
+        T[i] = 2*T[i-1]
+
+    if n >= 4:
+        return piastrellamento_di_tipo_A(n) + piastrellamento_di_tipo_B(n) + 2*T[n-1] + 4*(T[n-4])
+    else:
+        return piastrellamento_di_tipo_A(n) + piastrellamento_di_tipo_B(n) + 2*T[n-1]
