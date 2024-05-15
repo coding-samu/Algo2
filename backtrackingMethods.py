0c30dea9986d9eab6064473cec40b9ec10597ea5
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-05-15 15:23:23 +0200

    updated first backtracking method

--- backtrackingMethods.py
+++ backtrackingMethods.py
@@ -1,22 +1,52 @@
 def es(A,k):
-    def esR(A,k,n,sol,i=0,sum=0, countSolutions=[0]):
+    nodo = 0
+    countSolutions = 0
+    def esR(A,k,n,sol,i=0,sum=0):
+        nonlocal nodo
+        nonlocal countSolutions
+        nodo += 1
         if sum == k:
             print(sol)
-            countSolutions[0] += 1
+            countSolutions += 1
             return
         if i == n:
             return
         if sum + A[i] <= k:
             sol.append(A[i])
-            esR(A,k,n,sol,i+1,sum+A[i], countSolutions)
+            esR(A,k,n,sol,i+1,sum+A[i])
             sol.pop()
-        esR(A,k,n,sol,i+1,sum, countSolutions)
+        esR(A,k,n,sol,i+1,sum)
     
-    countSolutions = [0]
-    esR(A,k,len(A),[],0,0, countSolutions)
-    print(countSolutions[0])
+    esR(A,k,len(A),[],0,0)
+    print(countSolutions)
+    print(nodo)
+
+def esModificato(A,k):
+    nodo = 0
+    countSolutions = 0
+    def esR(A,k,n,sol,i=0,sum=0):
+        nonlocal nodo
+        nonlocal countSolutions
+        nodo += 1
+        if sum == k:
+            print(sol)
+            countSolutions += 1
+            return True
+        if i == n:
+            return False 
+        if sum + A[i] <= k:
+            sol.append(A[i])
+            if esR(A,k,n,sol,i+1,sum+A[i]): return True
+            sol.pop()
+        if esR(A,k,n,sol,i+1,sum): return True
+        return False
+    
+    esR(A,k,len(A),[],0,0)
+    print(countSolutions)
+    print(nodo)
 
 
 A = [61,20,1,33,20,2,4,1,1,33]
 k = 70
 es(A,k)
+esModificato(A,k)
