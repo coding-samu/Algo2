748b820ee7f965d9d7cfd8be3cc769ffbed76e20
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-05-17 00:49:22 +0200

    updated backtracking and added new test for backtracking algorithm and generated test images

--- backtrackingMethods.py
+++ backtrackingMethods.py
@@ -1,3 +1,10 @@
+import sys
+import time
+import numpy as np
+import matplotlib.pylab as plt
+
+sys.setrecursionlimit(20000)
+
 def es(A,k):
     nodo = 0
     countSolutions = 0
@@ -50,12 +57,8 @@ def esModificato(A,k):
 #es(A,k)
 #esModificato(A,k)
 
-import sys
-
-sys.setrecursionlimit(11000)
-
 def cerca_percorso(n):
-
+    soluzione = []
     def euristica(mossa, scacchiera):
         x, y = mossa
         combinazioni = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(-1,2),(1,-2),(-1,-2)]
@@ -64,7 +67,7 @@ def cerca_percorso(n):
 
     def ricorsiva(n,combinazioni,scacchiera,sol,x=0,y=0,i=0):
         if i == n*n-1:
-            print(sol, len(sol))
+            #print(sol, len(sol))
             return True
         combinazioni.sort(key=lambda mossa: euristica((x+mossa[0], y+mossa[1]), scacchiera))  # Ordina le mosse
         for px,py in combinazioni:
@@ -72,7 +75,7 @@ def cerca_percorso(n):
             py += y
             if px >= 0 and px < n and py >= 0 and py < n and scacchiera[py][px] == 0:
                 scacchiera[py][px]=1
-                sol.append((px,py))
+                sol.append((px,py,i+1))
                 if ricorsiva(n,combinazioni,scacchiera,sol,px,py,i+1):
                     return True
                 sol.pop()
@@ -81,9 +84,61 @@ def cerca_percorso(n):
     
     scacchiera = [[0 for _ in range(n)] for _ in range(n)]
     scacchiera[0][0] = 1
-    sol = [(0,0)]
+    sol = [(0,0,0)]
     combinazioni = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(-1,2),(1,-2),(-1,-2)]
-    return ricorsiva(n,combinazioni,scacchiera,sol)
+    t = ricorsiva(n,combinazioni,scacchiera,sol)
+    return sol,t
+
+def print_matrix(M):
+    for riga in M:
+        for colonna in riga:
+            print(colonna,end="\t")
+        print()
+
 
 if __name__ == '__main__':
-    print(cerca_percorso(14))
+    n = 132
+    # M = [[0 for _ in range(n)] for _ in range(n)]
+    # soluzione = cerca_percorso(n)
+    # for e in soluzione:
+    #     M[e[1]][e[0]] = e[2]
+    # print_matrix(M)
+
+
+
+    for i in range (2,26):
+        if i == 28 or i == 59 or i == 65 or i == 82 or i == 90 or i == 93 or i == 102 or i == 117 or i == 121 or i == 123 or i == 125: continue
+        print("Test metodo per i = " + str(i))
+        start = time.time()
+        soluzione,t = cerca_percorso(i)
+        end = time.time()
+        print(end-start)
+        print("Trovato per i = " + str(i) + " " + str(t))
+
+        if t:
+            M = [[0 for _ in range(i)] for _ in range(i)]
+            for e in soluzione:
+                M[e[1]][e[0]] = e[2]
+
+            M_np = np.array(M)
+
+            background = np.ones(M_np.shape)
+
+            plt.figure()
+            plt.matshow(background, fignum=0, cmap='binary')  # Mostra la matrice di sfondo
+
+            # Calcola la dimensione del testo in base alla dimensione della matrice
+            fontsize = max(20 - M_np.shape[0], 5)
+
+            for x in range(M_np.shape[0]):
+                for y in range(M_np.shape[1]):
+                    plt.text(y, x, str(M_np[x, y]), ha='center', va='center', color='black', fontsize=fontsize)
+
+            plt.xlim(-0.5,i-0.5)
+            plt.ylim(-0.5,i-0.5)
+            plt.gca().xaxis.tick_top()
+            plt.gca().invert_yaxis()
+            plt.title('Scacchiera ' + str(i) + 'x' + str(i))
+            plt.savefig('esImages/' + str(i) + '.png')
+
+            plt.close()
