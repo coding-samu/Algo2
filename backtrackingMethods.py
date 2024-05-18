2ec382682083d92bb6f555d34fe6acbbebc88e89
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-05-18 17:39:32 +0200

    updated backtracking methods

--- backtrackingMethods.py
+++ backtrackingMethods.py
@@ -1,10 +1,3 @@
-import sys
-import time
-import numpy as np
-import matplotlib.pylab as plt
-
-sys.setrecursionlimit(20000)
-
 def es(A,k):
     nodo = 0
     countSolutions = 0
@@ -57,73 +50,68 @@ def esModificato(A,k):
 #es(A,k)
 #esModificato(A,k)
 
-def cerca_percorso(n):
-    soluzione = []
-    def euristica(mossa, scacchiera):
-        x, y = mossa
-        combinazioni = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(-1,2),(1,-2),(-1,-2)]
-        mosse_valide = [(x+px, y+py) for px, py in combinazioni if 0 <= x+px < n and 0 <= y+py < n and scacchiera[y+py][x+px] == 0]
-        return len(mosse_valide)
+def percorsiMatrice(M):
+    n = len(M)
 
-    def ricorsiva(n,combinazioni,scacchiera,sol,x=0,y=0,i=0):
-        if i == n*n-1:
-            #print(sol, len(sol))
-            return True
-        combinazioni.sort(key=lambda mossa: euristica((x+mossa[0], y+mossa[1]), scacchiera))  # Ordina le mosse
-        for px,py in combinazioni:
-            px += x
-            py += y
-            if px >= 0 and px < n and py >= 0 and py < n and scacchiera[py][px] == 0:
-                scacchiera[py][px]=1
-                sol.append((px,py,i+1))
-                if ricorsiva(n,combinazioni,scacchiera,sol,px,py,i+1):
-                    return True
+    def esplora(M,i,j,sol):
+        # Se sono arrivato all'ultima riga, stampo sol
+        if i == n-1:
+            print(sol)
+            return
+        # Altrimenti:
+        else:
+            # Vado giù
+            sol.append(M[i+1][j])
+            esplora(M,i+1,j,sol)
+            sol.pop()
+            # Vado giù a sinistra
+            if j > 0:
+                sol.append(M[i+1][j-1])
+                esplora(M,i+1,j-1,sol)
                 sol.pop()
-                scacchiera[py][px]=0
-        return False
-    
-    scacchiera = [[0 for _ in range(n)] for _ in range(n)]
-    scacchiera[0][0] = 1
-    sol = [(0,0,0)]
-    combinazioni = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(-1,2),(1,-2),(-1,-2)]
-    t = ricorsiva(n,combinazioni,scacchiera,sol)
-    return sol,t
+            # Vado giù a destra
+            if j < n-1:
+                sol.append(M[i+1][j+1])
+                esplora(M,i+1,j+1,sol)
+                sol.pop()
+
+    for j in range(n):
+        sol = []
+        sol.append(M[0][j])
+        esplora(M,0,j,sol)
+        sol.pop()
+
+def numeroTelefonico(n):
+    T = [[0 for _ in range(10)] for _ in range(n+1)]
+
+    for i in range(n+1):
+        for j in range(10):
+            if i == 0:
+                T[i][j] = 0
+            elif i == 1:
+                T[i][j] = 1
+            elif j == 0:
+                T[i][j] = T[i-1][8]
+            elif j == 1:
+                T[i][j] = T[i-1][2] + T[i-1][4]
+            elif j == 2:
+                T[i][j] = T[i-1][1] + T[i-1][3] + T[i-1][5]
+            elif j == 3:
+                T[i][j] = T[i-1][2] + T[i-1][6]
+            elif j == 4:
+                T[i][j] = T[i-1][1] + T[i-1][5] + T[i-1][7]
+            elif j == 5:
+                T[i][j] = T[i-1][2] + T[i-1][4] + T[i-1][6] + T[i-1][8]
+            elif j == 6:
+                T[i][j] = T[i-1][3] + T[i-1][5] + T[i-1][9]
+            elif j == 7:
+                T[i][j] = T[i-1][4] + T[i-1][8]
+            elif j == 8:
+                T[i][j] = T[i-1][5] + T[i-1][7] + T[i-1][9] + T[i-1][0]
+            elif j == 9:
+                T[i][j] = T[i-1][6] + T[i-1][8]
+
+    return sum(T[n])
 
 if __name__ == '__main__':
-
-    for i in range (7,8):
-        if i == 28 or i == 59 or i == 65 or i == 82 or i == 90 or i == 93 or i == 102 or i == 117 or i == 121 or i == 123 or i == 125: continue
-        print("Test metodo per i = " + str(i))
-        start = time.time()
-        soluzione,t = cerca_percorso(i)
-        end = time.time()
-        print(end-start)
-        print("Trovato per i = " + str(i) + " " + str(t))
-
-        if t:
-            M = [[0 for _ in range(i)] for _ in range(i)]
-            for e in soluzione:
-                M[e[1]][e[0]] = e[2]
-
-            M_np = np.array(M)
-
-            background = np.ones(M_np.shape)
-
-            plt.figure()
-            plt.matshow(background, fignum=0, cmap='binary')  # Mostra la matrice di sfondo
-
-            # Calcola la dimensione del testo in base alla dimensione della matrice
-            fontsize = max(20 - M_np.shape[0], 5)
-
-            for x in range(M_np.shape[0]):
-                for y in range(M_np.shape[1]):
-                    plt.text(y, x, str(M_np[x, y]), ha='center', va='center', color='black', fontsize=fontsize)
-
-            plt.xlim(-0.5,i-0.5)
-            plt.ylim(-0.5,i-0.5)
-            plt.gca().xaxis.tick_top()
-            plt.gca().invert_yaxis()
-            plt.title('Scacchiera ' + str(i) + 'x' + str(i))
-            plt.savefig('esImages/' + str(i) + '.png')
-
-            plt.close()
+    print(numeroTelefonico(10))
